17936b78a83043147f36da286dae997f09c1e632
Ritin-Dev <gitprojectritin@gmail.com>
2025-03-20 15:50:55 +0530

    Update breadcrumb.js

--- src/js/breadcrumb.js
+++ src/js/breadcrumb.js
@@ -31,8 +31,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    let span = document.querySelector("nav#breadcrumb span, a");
+    let span = document.querySelector("nav#breadcrumb span");
+    let anchor = document.querySelector("nav#breadcrumb a");
     if (span) {
         span.textContent = span.textContent.replace(/-/g, " ");
+    }
+    if (anchor) {
+        anchor.textContent = anchor.textContent.replace(/-/g, " ");
     }
 });
